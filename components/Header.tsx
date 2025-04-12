0345b38c3b63c48015e7af1b281be5382e55a272
marketlube dev <marketlubedevelopers@gmail.com>
2025-04-12 19:26:02 +0530

    sl

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,69 +1,81 @@
-"use client"
+"use client";
 
-import Image from 'next/image'
-import Link from 'next/link'
-import React, { useState } from 'react'
-import { usePathname } from 'next/navigation'
-import Sidebar from './Sidebar'
-
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import { usePathname } from "next/navigation";
+import Sidebar from "./Sidebar";
 
 const Header = () => {
-    const pathname = usePathname()
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const pathname = usePathname();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
-    const closeSidebar = () => {
-        setIsSidebarOpen(false);
-    };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
 
-    const navLinks = [
-        {title: "Programs", link: "/programs"},
-        {title: "Destination", link: "/destination"},
-        {title: "Services", link: "/#services"},
-        {title: "About us", link: "/#about-us"},
-        // {title: "Contact", link: ""},
-    ]
+  const navLinks = [
+    { title: "Programs", link: "/programs" },
+    { title: "Destination", link: "/destination" },
+    { title: "Services", link: "/#services" },
+    { title: "About us", link: "/#about-us" },
+    // {title: "Contact", link: ""},
+  ];
 
   return (
     <header>
-        <div className = "container navbar">
-            <Link href = "/">
-                <Image src = "/images/Logo.svg" alt = "logo" height={40} width={151}/>
-            </Link>
+      <div className="container navbar">
+        <Link href="/">
+          <Image src="/images/Logo.svg" alt="logo" height={40} width={151} />
+        </Link>
 
-            <nav>
-                <ul>
-                    {navLinks.map((item, index) => (
-                        <li key = {index}>
-                            <Link href = {item.link} className={pathname === item.link?"active-link":""}>{item.title}</Link>
-                        </li>
-                    ))}
-                    <li>
-                        <Link href="//api.whatsapp.com/send?phone=919895101101&text=Hi" target='_blank'>Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Link href="//api.whatsapp.com/send?phone=919895101101&text=Hi" target='_blank'><button>
-                Register Now
-            </button></Link>
+        <nav>
+          <ul>
+            {navLinks.map((item, index) => (
+              <li key={index}>
+                <Link
+                  href={item.link}
+                  className={pathname === item.link ? "active-link" : ""}
+                >
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+            <li>
+              <Link
+                href="//api.whatsapp.com/send?phone=919895101101&text=Hi"
+                target="_blank"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Link
+          href="//api.whatsapp.com/send?phone=919895101101&text=Hi"
+          target="_blank"
+        >
+          <button>Register Now</button>
+        </Link>
 
-            <Image 
-                src = "/images/icons/Menu.svg" 
-                alt = "hamburger" 
-                height = {50} 
-                width = {50} 
-                id = "hamburger"
-                onClick={toggleSidebar}
-                style={{ cursor: 'pointer' }}
-            />
-        </div>
-        
-        <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
+        <Image
+          src="/images/icons/Menu.svg"
+          alt="hamburger"
+          height={50}
+          width={50}
+          id="hamburger"
+          onClick={toggleSidebar}
+          style={{ cursor: "pointer" }}
+        />
+      </div>
+
+      <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
