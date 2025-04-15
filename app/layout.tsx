9253cab70715acfef8e1fc5145a11264b1e64608
marketlube dev <marketlubedevelopers@gmail.com>
2025-04-15 17:23:51 +0530

    change address and fav

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,33 +5,34 @@ import { Inter, Poppins, Bad_Script, Podkova } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 
 const badScript = Bad_Script({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
 });
 
-const inter = Inter({ 
-  subsets: ['latin'],
-  variable: '--font-inter'
-})
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
-const podkova = Podkova({ 
-  weight: '400',
-  subsets: ['latin'],
-  variable: '--font-podkova'
-})
+const podkova = Podkova({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-podkova",
+});
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-poppins'
-})
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
 
-console.log(badScript, podkova)
+console.log(badScript, podkova);
 
 export const metadata: Metadata = {
-  title: "PEF Education",
-  description: "Turn your study abroad dream into reality. Explore top universities, experience new cultures, and build a future without limits.",
+  title: "PEF Education | Give wings to your dreams",
+  description:
+    "Turn your study abroad dream into reality. Explore top universities, experience new cultures, and build a future without limits.",
 };
 
 export default function RootLayout({
@@ -41,6 +42,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${poppins.variable}`}>
+      <head>
+        <link rel="icon" href="/images/icons/bird.png" type="image/png" />
+      </head>
       <body>
         <Header />
         {children}
