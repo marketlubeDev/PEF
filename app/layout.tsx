a1afba4b015bdbfb7ca0c85b5f5dd937bf101527
marketLube <marketlubes@gmail.com>
2025-05-05 14:28:48 +0530

    SL

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,25 @@
 import type { Metadata } from "next";
 import "../styles/CSS/global-min.css";
 import Header from "@/components/Header";
-import { Inter, Poppins, Bad_Script, Podkova } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 
-const badScript = Bad_Script({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-});
+// const badScript = Bad_Script({
+//   weight: "400",
+//   subsets: ["latin"],
+//   display: "swap",
+// });
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
 
-const podkova = Podkova({
-  weight: "400",
-  subsets: ["latin"],
-  variable: "--font-podkova",
-});
+// const podkova = Podkova({
+//   weight: "400",
+//   subsets: ["latin"],
+//   variable: "--font-podkova",
+// });
 
 const poppins = Poppins({
   subsets: ["latin"],
